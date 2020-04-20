8b7ca68da1703cdb861c9c0e592a82cec58c0a29
pluboj <pluboj@yahoo.com>
2020-04-19 20:37:18 -0700

    Data map

--- world_population.py
+++ world_population.py
@@ -1,11 +1,26 @@
 import json
+import pygal.maps.world
+
+from country_codes import get_country_code
 
 filename = 'population_json.json'
 with open(filename) as f:
 	pop_data = json.load(f)
 
+cc_populations = {}
+
 for pop_dict in pop_data:
 	if pop_dict['Year'] == 2010:
 		country_name = pop_dict['Country Name']
 		population = int(float(pop_dict['Value']))
-		print(country_name + ": " + str(population))
+		code = get_country_code(country_name)
+		if code:
+			cc_populations[code] = population
+		else:
+			print('ERROR - ' + country_name)
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2018, by Country'
+wm.add('2018', cc_populations)
+
+wm.render_to_file('world_population.svg')
